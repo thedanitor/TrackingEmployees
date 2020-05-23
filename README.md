b8562215b95bfadd4c0c8b0e15330fda5cada250
Dan Weikart <danmweikart@gmail.com>
2020-05-22 18:01:51 -0700

    Updated README

--- README.md
+++ README.md
@@ -1,13 +1,37 @@
 # TrackingEmployees
 
-## Description
-
 I made a command line application for keeping track of employees.
 
 GIF HERE
 
+## Table of Contents
+
+* [Description](#description)
+* [Technologies Used](#technologies)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+
+## Description
+
+
+
+
+
+
 ## Technologies Used
 
-*MySQL was used to create the database and tables
-*node inquirer was used to create the command line interface
-*
+* MySQL was used to create the database and tables
+* node inquirer and console table were used to create the command line interface
+* JavaScript was used to create the functions
+
+## Installation
+
+* To install the necessary dependencies run npm i on the command line.
+* User will need to create a .env file and enter their environment variables
+* Finally copy the schema and seed data into MySQL and run to create the database
+* Now application is ready to use!
+
+## Usage
+
+Navigate to the relevant repository and enter either run npm start or node server.js on the command line to start the application.
--- seed.sql
+++ seed.sql
@@ -1,31 +1,4 @@
-DROP DATABASE IF EXISTS trackerDB;
-CREATE DATABASE trackerDB;
-USE trackerDB;
-
-CREATE TABLE department (
-id INTEGER(11) AUTO_INCREMENT NOT NULL,
-name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE role (
-id INTEGER(11) AUTO_INCREMENT NOT NULL,
-title VARCHAR(30) NOT NULL,
-salary DECIMAL(10,2) NOT NULL,
-department_id INTEGER(5),
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE employee (
-id INTEGER(11) AUTO_INCREMENT NOT NULL,
-first_name VARCHAR(30) NOT NULL,
-last_name VARCHAR(30) NOT NULL,
-role_id INTEGER (5),
-manager_id INTEGER (5),
-  PRIMARY KEY (id)
-);
-
--- Add department
+-- Add to department
 INSERT INTO department (name)
 VALUES 
 ("Delivery"), 
@@ -33,7 +6,7 @@ VALUES
 ("Human Resources"), 
 ("Medical");
 
--- Add role
+-- Add to role
 INSERT INTO role (title, salary, department_id)
 VALUES
 ("Delivery Boy", 30000, 1), 
@@ -43,7 +16,7 @@ VALUES
 ("Intern", 15000, 3), 
 ("Bureaucrat", 80000, 3);
 
--- Add employee
+-- Add to employee
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
 ("Philip J.", "Fry", 1, 2), 
